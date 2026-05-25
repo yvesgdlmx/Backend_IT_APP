5f72c3b8f992c1820a596f0689a5d358ce4f06a4
yvesgdlmx <gerencia.tecnologias@optimex.mx>
2026-05-25 12:15:54 -0600

    ajsustes

--- helpers/enviarCorreo.js
+++ helpers/enviarCorreo.js
@@ -19,11 +19,28 @@ const crearTransporter = () => {
   });
 };
 
+const obtenerRemitente = () => {
+  const address = process.env.SMTP_FROM_ADDRESS || process.env.SMTP_USER;
+  const name = process.env.SMTP_FROM_NAME || "TI CONTROL";
+
+  if (!address) {
+    throw new Error("Falta configurar SMTP_FROM_ADDRESS o SMTP_USER.");
+  }
+
+  return { name, address };
+};
+
 export const enviarCorreo = async ({ to, subject, html, text }) => {
   const transporter = crearTransporter();
+  const remitente = obtenerRemitente();
 
   return transporter.sendMail({
-    from: process.env.SMTP_FROM || process.env.SMTP_USER,
+    from: remitente,
+    sender: process.env.SMTP_USER,
+    envelope: {
+      from: process.env.SMTP_USER,
+      to,
+    },
     to,
     subject,
     html,
